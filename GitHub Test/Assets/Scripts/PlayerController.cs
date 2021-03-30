19512a6c7e987807692acb6dbbf94c1a774097b7
MrPasimio <jpasimio@mvsd320.org>
2021-03-30 08:01:41 -0700

    First Build
    Whiteball moves now

---
+++ GitHub Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    public float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right *horizontalInput* Time.deltaTime * moveSpeed);
+        transform.Translate(Vector2.up *verticalInput* Time.deltaTime * moveSpeed);
+
+    }
+}
